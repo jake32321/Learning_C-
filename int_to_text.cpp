12507e00a4b45b3be8f3175be86fea628f14b86b
Jacob Reed <jake32321@yahoo.com>
2015-10-07 23:14:37 -0500

    another file to commit

---
+++ int_to_text.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    
+    if(1<=n<=9){
+        if(n==1)
+            cout<<"one"<<endl;
+        if(n==2)
+            cout<<"two"<<endl;
+        if(n==3)
+            cout<<"three"<<endl;
+        if(n==4)
+            cout<<"four"<<endl;
+        if(n==5)
+            cout<<"five"<<endl;
+        if(n==6)
+            cout<<"six"<<endl;
+        if(n==7)
+            cout<<"seven"<<endl;
+        if(n==8)
+            cout<<"eight"<<endl;
+        if(n==9)
+            cout<<"nine"<<endl;
+    }else{
+        cout<<"Greater than 9"<<endl;
+    }
+   return 0;
+}
