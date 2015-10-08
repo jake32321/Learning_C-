1fa685070fd5959f39133d50bc63b4f47da04b39
Jacob Reed <jake32321@yahoo.com>
2015-10-08 10:05:36 -0500

    added new project file

---
+++ array_adder.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    //Scans a given array to find the sum 
+    int sum=0, n;
+    int array[100];
+    scanf("%d", &n);
+    
+    //Adds elements to the sum
+    for(int i=0; i<n; i++){
+        scanf("%d", &array[i]);
+        sum=sum+array[i];
+    }
+    //Returns sum
+    printf("%d", sum);
+    return 0;
+}
+
