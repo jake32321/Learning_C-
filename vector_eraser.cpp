a0ff71a73d9f0e2fb49cd108da8a085f9ae6f652
Jacob Reed <jake32321@yahoo.com>
2015-10-19 22:09:47 -0500

    Added documentation to vector_eraser

--- vector_eraser.cpp
+++ vector_eraser.cpp
@@ -6,22 +6,21 @@
 using namespace std;
 
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+int main() {  
     int in, in_two, in_three, in_four, element;
-    cin>>in;
+    cin>>in; //Takes in the size of the vector
     vector<int> ints;
-    for(int i=0; i<in; i++){
+    for(int i=0; i<in; i++){ //Adds values to the vector from an input
         scanf("%d", &element);
         ints.push_back(element);
     }
-    cin>>in_two;
+    cin>>in_two; //Takes in a single value to be removed
     ints.erase(ints.begin()+(in_two-1));
-    cin>>in_three;
-    cin>>in_four;
-    ints.erase(ints.begin()+(in_three-1), ints.begin()+(in_four-1));
+    cin>>in_three; //Takes in the inclusive starting index 
+    cin>>in_four; //Takes in the exclusive end index
+    ints.erase(ints.begin()+(in_three-1), ints.begin()+(in_four-1));  //Erases from starting to end value
     cout<<ints.size()<<endl;
-    for(int i=0; i<ints.size(); i++){
+    for(int i=0; i<ints.size(); i++){  //Prints values to the console
         cout<<ints[i]<<" ";
     }
     return 0;
